4a46a3ca1337f39caa0971e49a529c360ce1640a
alaa7731 <alaa7731@gmail.com>
2017-01-20 21:44:18 +0300

    creating project

---
+++ app/src/main/java/tigerspike/andorid/mygallery/ImagesGallery.java
@@ -0,0 +1,13 @@
+package tigerspike.andorid.mygallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImagesGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images_gallery);
+    }
+}
